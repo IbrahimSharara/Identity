bea8cfa0509c37eac54963af4e851aecc8e9b485
IbrahimSharara <ibrahem.gamal1998@gmail.com>
2023-07-11 11:31:16 +0300

    initial create

---
+++ UserManagementWithidentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserManagementWithidentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
